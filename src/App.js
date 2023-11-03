65c88bc5940e44f61c7aa76fa983f3706b106952
willyongaks <134366767+Devongaki@users.noreply.github.com>
2023-11-03 15:20:44 +0100

    hero image

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { Navigation } from './component/Nav/Nav';
-import { HomePage } from './component/pages/Home/Home';
-import { GamesPage } from './component/pages/Games/Games';
-import { AboutPage } from './component/pages/About/About';
+import { Navigation } from "./component/Nav/Nav";
+import { HomePage } from "./component/pages/Home/Home";
+import { GamesPage } from "./component/pages/Games/Games";
+import { AboutPage } from "./component/pages/About/About";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/games" element={<GamesPage />} />
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/games" element={<GamesPage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
     </BrowserRouter>
   );
 }
--- src/component/pages/Home/Home.jsx
+++ src/component/pages/Home/Home.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import "./index.css";
+import backgroundImage from "../../../assets/images/desktop/image-hero.jpg";
 
 export const HomePage = () => {
   return (
-    <div className='container'>HomePage</div>
-  )
-}
+    <div className="container">
+      <div className="hero_img">
+        <img src={backgroundImage} alt="" />
+      </div>
+    </div>
+  );
+};
