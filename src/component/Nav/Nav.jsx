7dd6d2c2751d04ef1ffb24a766965191da6b3abf
willyongaks <134366767+Devongaki@users.noreply.github.com>
2023-11-08 11:25:45 +0100

    nav

--- src/component/Nav/Nav.jsx
+++ src/component/Nav/Nav.jsx
@@ -1,43 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./index.css";
-import { FaTimes } from "react-icons/fa";
-import { FaBars } from "react-icons/fa";
-
 
 export const Navigation = () => {
-  
-  
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <div className="container">
-      <nav>
-        <div className="logo">
-          <Link to="/">Gamestudios</Link>
-        </div>
-        <div className="nav_links">
+    <nav>
+      <Link to="/" className="logo">
+        Gamestudios
+      </Link>
+      <div
+        className="menu_btns"
+        onClick={() => {
+          setIsMenuOpen(!isMenuOpen);
+        }}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={isMenuOpen ? "open" : ""}>
+        <li>
           <NavLink to={`/`} className="nav_links-items">
             Home
           </NavLink>
+        </li>
+        <li>
           <NavLink to={`/games`} className="nav_links-items">
             Games
           </NavLink>
+        </li>
+        <li>
           <NavLink to="/about" className="nav_links-items">
             About
           </NavLink>
+        </li>
+        <li>
           <NavLink to="/" className="nav_links-items">
             SearchBox
           </NavLink>
-        </div>
-
-        <div className="menu_btns">
-          <div className="menu_btns-close">
-            <FaBars />
-          </div>
-          <div className="menu_btns-open">
-            <FaTimes />
-          </div>
-        </div>
-      </nav>
-    </div>
+        </li>
+      </ul>
+    </nav>
   );
 };
