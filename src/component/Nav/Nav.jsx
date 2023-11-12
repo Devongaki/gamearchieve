c98346b6c715692ea8d3f3ffd8e98c1122825eb4
willyongaks <134366767+Devongaki@users.noreply.github.com>
2023-11-12 15:14:13 +0100

    Update Nav.jsx

--- src/component/Nav/Nav.jsx
+++ src/component/Nav/Nav.jsx
@@ -5,13 +5,6 @@ import "./index.css";
 export const Navigation = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  // Assuming you have a list of games
-  const games = [
-    { id: 1, title: "Game 1" },
-    { id: 2, title: "Game 2" },
-    // Add more games as needed
-  ];
-
   return (
     <div className="nav_container">
       <nav className="container">
@@ -39,16 +32,14 @@ export const Navigation = () => {
               Games
             </NavLink>
           </li>
-          {games.map((game) => (
-            <li key={game.id}>
+        
+            <li>
               <NavLink
-                to={`/game-details/${game.id}`}
+                to={`/game-details`}
                 className="nav_links-items"
               >
-                {game.title}
               </NavLink>
             </li>
-          ))}
         </ul>
       </nav>
     </div>
