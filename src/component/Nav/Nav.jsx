612b79cfba62231401d3812d8d25e67cfe2cb097
willyongaks <134366767+Devongaki@users.noreply.github.com>
2023-11-12 16:17:36 +0100

    Update Nav.jsx

--- src/component/Nav/Nav.jsx
+++ src/component/Nav/Nav.jsx
@@ -1,12 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./index.css";
 
 export const Navigation = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navigationRef = useRef(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      // Close the menu if the click is outside the navigation container
+      if (
+        navigationRef.current &&
+        !navigationRef.current.contains(event.target)
+      ) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    // Add event listener to detect clicks outside the navigation container
+    document.addEventListener("click", handleOutsideClick);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
 
   return (
-    <div className="nav_container">
+    <div className="nav_container" ref={navigationRef}>
       <nav className="container">
         <Link to="/" className="logo">
           Gamestudios
